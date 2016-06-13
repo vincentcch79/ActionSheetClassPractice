9c03d567d31fec11ec0c20cbcf3a797854f14473
Vincent Chang <vincentcch79@gmail.com>
2016-06-13 14:39:09 +0800

    Afterclass exercise one

--- AlertTest/ViewController.swift
+++ AlertTest/ViewController.swift
@@ -21,5 +21,34 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func showAlert(sender: AnyObject) {
+        let alert = UIAlertController(title: "Alert", message: "想談戀愛嗎？", preferredStyle: .Alert)
+        
+        let cancelAction = UIAlertAction(title: "No", style: .Cancel, handler: nil)
+         alert.addAction(cancelAction)
+        
+        
+        
+        
+        
+        let okActionHandler = { (action: UIAlertAction!) -> Void in
+            let okOption = UIAlertController(title: "你想跟誰談戀愛？", message: "在以下的人選中選一個你想談戀愛的對象", preferredStyle: .ActionSheet)
+            okOption.addAction(UIAlertAction(title: "Kate Upton", style: .Default, handler: nil))
+            okOption.addAction(UIAlertAction(title: "Emily Clarke", style: .Default, handler: nil))
+            okOption.addAction(UIAlertAction(title: "Candice Swanepoel", style: .Default, handler: nil))
+            okOption.addAction(UIAlertAction(title: "Clara Lee", style: .Default, handler: nil))
+            okOption.addAction(UIAlertAction(title: "Kim Hyuna", style: .Default, handler: nil))
+            self.presentViewController(okOption, animated: true, completion: nil)
+        }
+        
+        
+        
+        
+        let okAction = UIAlertAction(title: "Yes", style: .Default, handler: okActionHandler)
+        alert.addAction(okAction)
+        
+        self.presentViewController(alert, animated: true, completion: nil)
+        
+    }
 }
 
